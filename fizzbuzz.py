0689c8f8edd5399481f877509d61a8654f45df04
Sam20B <wafulabibiana93@gmail.com>
2023-10-24 00:01:40 +0300

    code canvas

--- fizzbuzz.py
+++
@@ -1,9 +0,0 @@
-for num in range(45, 1050):
-    if num % 2 == 0:
-        print("fizz")
-    if num % 5 ==0:
-        print("buzz")
-    if num % 10 == 0:
-        print("fizzbuzz")
-    else:
-        print(num)
